a6bd3571fc3d49105e8ae792f3a6936c8c039c5a
Ankitkumar Prajapati <42886562+Ankit13p@users.noreply.github.com>
2020-10-27 15:19:28 +1300

    pet appointment tool

---
+++ build/precache-manifest.c1efb2a91bd449c5a446da49882d4633.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10924b2410ceb848ea802f171ede8aa4",
+    "url": "/appointment/index.html"
+  },
+  {
+    "revision": "215ff392600ed52ef766",
+    "url": "/appointment/static/css/2.e712defa.chunk.css"
+  },
+  {
+    "revision": "0044b4c0a7c536969d2f",
+    "url": "/appointment/static/css/main.113080c1.chunk.css"
+  },
+  {
+    "revision": "215ff392600ed52ef766",
+    "url": "/appointment/static/js/2.2156b28e.chunk.js"
+  },
+  {
+    "revision": "acb364211461e18f66aec1e8c68e542c",
+    "url": "/appointment/static/js/2.2156b28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0044b4c0a7c536969d2f",
+    "url": "/appointment/static/js/main.fb73bf08.chunk.js"
+  },
+  {
+    "revision": "a5ae47a1133683ec21de",
+    "url": "/appointment/static/js/runtime-main.1f88db5d.js"
+  }
+]);
